47e4d94b35bf48a76360af0c29f74f5dfaedc3ba
Ignas-Jermakovicius-Prog <114738526+Ignas-Jermakovicius-Prog@users.noreply.github.com>
2022-10-28 18:06:33 +0300

    Update V.03.cpp

--- V.03.cpp
+++ V.03.cpp
@@ -1,85 +1,49 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <iomanip>
-#include <fstream>
-#include <sstream>
-#include <iterator>
-#include <stdlib.h>
-#include <numeric>
-#include <chrono>
-#include "genrandom.h"
-#include "readFile.h"
-#include "genpaz.h"
-#include "countgal.h"
+#include "vidmed.h"
 #include "generavimas.h"
+#include "build_lib.h"
 
 using namespace std;
 
 
 int main()
 {
-	vector<int> skaiciai;
-	int kiek = generavimas(skaiciai);
-	
-	vector<stud> studentai;
-	readFile(studentai, kiek);
 
-	vector<stud> talentai;
-	vector<stud> nuskriaustieji;
-	int tal = 0;
-	int nusk = 0;
+	int kiek = 1000;
+	for (int i = 0; i < 5; i++)
+	{
+		auto startviso = chrono::high_resolution_clock::now();
+		failogeneravimas(kiek);
+		vector<stud> studentai;
+		int pazskc;
+		skaitymas(studentai, "studentai_" + to_string(kiek) + ".txt", &pazskc);
 
-	auto start = chrono::high_resolution_clock::now();
-	auto st = start;
-	for (int i = 0; i < kiek; i++) {
-		float paz = 5.00;
-		if (studentai.at(i).gal >= paz) {
-			talentai.push_back(studentai.at(i));
-			tal++;
+		auto start = high_resolution_clock::now();
+
+		vector<stud> talentai;
+		vector<stud> nuskriaustieji;
+
+		for (int i = 0; i < studentai.size(); i++)
+		{
+			stud laikstud = studentai[i];
+			vid(laikstud);
+			med(laikstud);
+			
+			if (laikstud.gal >= 5)
+				talentai.push_back(laikstud);
+			else
+				nuskriaustieji.push_back(laikstud);
 		}
-	}
-	for (int i = 0; i < kiek; i++) {
-		float paz = 5.00;
-		if (studentai.at(i).gal < paz) {
-			nuskriaustieji.push_back(studentai.at(i));
-			nusk++;
-		}
-	}
-	
-	auto end = chrono::high_resolution_clock::now();
-	chrono::duration<double> diff = end - start;
-	cout << to_string(kiek) + " studentu rusiavimas i 2 grupes uztruko : " << diff.count() << " s\n";
+		auto end = high_resolution_clock::now();
+		duration<double> diff = end - start;
+		cout << "Studentu rusiavimas i 2 grupes uztruko : " << diff.count() << "s\n";
+		
+		isvedimas("talentai" + to_string(kiek) + ".txt", talentai);
+		isvedimas("nuskriaustieji" + to_string(kiek) + ".txt", nuskriaustieji);
 
-	string pav;
-
-	pav = "nuskriaustieji_" + to_string(kiek) + ".txt";
-	ofstream nuskduom(pav);
-	auto start1 = chrono::high_resolution_clock::now();
-	auto st1 = start1;
-	for (int i = 0; i < kiek; i++) {
-
-		float paz = 5.00;
-		if (studentai.at(i).gal < paz) {
-			nuskduom << studentai.at(i).Vard << setw(20) << studentai.at(i).Pav << setw(18) << studentai.at(i).gal << endl;
-		}
+		auto endviso = high_resolution_clock::now();
+		duration<double> diffviso = endviso - startviso;
+		cout << "Testas su " << kiek << " studentu uztruko: " << diffviso.count() << "s" << endl << endl;
+		kiek *= 10;
 	}
-	auto end1 = chrono::high_resolution_clock::now();
-	chrono::duration<double> diff1 = end1 - start1;
-	cout << to_string(kiek) + " nuskriaustuju studentu irasymas i faila uztruko : " << diff1.count() << " s\n";
-
-	pav = "talentai_" + to_string(kiek) + ".txt";
-	ofstream talduom(pav);
-	auto start2 = chrono::high_resolution_clock::now();
-	auto st2 = start2;
-	for (int j = 0; j < kiek; j++) {
-		float paz = 5.00;
-		if (studentai.at(j).gal >= paz) {
-			talduom << studentai.at(j).Vard << setw(20) << studentai.at(j).Pav << setw(18) << studentai.at(j).gal << endl;
-		}
-	}
-	auto end2 = chrono::high_resolution_clock::now();
-	chrono::duration<double> diff2 = end2 - start2;
-	cout << to_string(kiek) + " talentu studentu irasymas i faila : " << diff2.count() << " s\n";
-	
+	cout << endl;
 }

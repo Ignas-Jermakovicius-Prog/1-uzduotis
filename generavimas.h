1c361a83596b480e354cbab1a911e21814bb9cda
Ignas-Jermakovicius-Prog <114738526+Ignas-Jermakovicius-Prog@users.noreply.github.com>
2022-11-17 01:22:21 +0200

    Update generavimas.h

--- generavimas.h
+++ generavimas.h
@@ -6,11 +6,9 @@ using namespace std;
 
 int countWords(string const& str);
 int randompaz();
-void autopaz(vector<stud>& S, int i, int pazkiek);
-void input(vector<stud>& S, int i);
 bool has_digit(string s);
-void name_input(vector<stud>& S, int i);
+void input(vector<stud>& S, int i);
 bool compare(const stud& a, const stud& b);
 void failogeneravimas(int kiek);
 void skaitymas(vector<stud>& S, string failo_pavadinimas, int* kiek);
-void isvedimas(string failo_pavadinimas, vector<stud> vek);
+void isvedimas(string failo_pavadinimas, vector<stud> S);
